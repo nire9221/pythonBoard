e1063ae82880f1ce1f0fb92e71f074b566473625
erin <nire9221@gmail.com>
2020-03-01 12:42:12 -0800

    update

---
+++ itBoardProject/itBoardApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ItboardappConfig(AppConfig):
+    name = 'itBoardApp'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/erin/win2020/itc172/final project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
