96e0f023b1ff5774245c6902ce08f6f4dfba07f0
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-03-02 17:52:23 -0800

    adds model for Jobs

--- itBoardProject/itBoardApp/models.py
+++ itBoardProject/itBoardApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Job(models.Model):
+    JobTitle=models.CharField(max_length=255)
+    Employer=models.CharField(max_length=255)
+    JobURL=models.URLField(null=True, blank=True)
+    User=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    JobLevel=models.CharField(max_length=255)
+    Location=models.CharField(max_length=255)
+    Availability =models.DateField()
+    Description=models.TextField()
+
+    def __str__(self):
+        return self.JobTitle
+    
+    class Meta:
+        db_table='Job'
+        verbose_name_plural='Jobs'
