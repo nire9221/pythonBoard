389b141d5ee28f4fb5fc92e429409ba0291d021a
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-03-22 13:43:47 -0700

    Add files via upload

--- itBoardProject/itBoardProject/urls.py
+++ itBoardProject/itBoardProject/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('itBoardApp/', include('itBoardApp.urls'))
+    path('itBoardApp/', include('itBoardApp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
